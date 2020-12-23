61e879be57613121d171e91ffb96c165fe1d5442
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2020-12-23 00:19:04 -0400

    updates

--- app.js
+++ app.js
@@ -1,85 +1,94 @@
 const express = require('express')
 const fs = require('fs')
 const app = express()
-//this line is required to parse the request body
+
+
+//esta linha é necessária para analisar o corpo da solicitação
 app.use(express.json())
-/* Create - POST method */
+/* create user*/
 app.post('/user/add', (req, res) => {
-    //get the existing user data
+    //obter os dados existentes do usuário
     const existUsers = getUserData()
     
-    //get the new user data from post request
+    //obter os novos dados do usuário na pós-solicitação
     const userData = req.body
-    //check if the userData fields are missing
+    //verifique se os campos userData estão faltando
     if (userData.fullname == null || userData.age == null || userData.username == null || userData.password == null) {
         return res.status(401).send({error: true, msg: 'User data missing'})
     }
     
-    //check if the username exist already
+    //verifique se o nome de usuário já existe
     const findExist = existUsers.find( user => user.username === userData.username )
     if (findExist) {
         return res.status(409).send({error: true, msg: 'username already exist'})
     }
-    //append the user data
+    //anexar os dados do usuário
     existUsers.push(userData)
-    //save the new user data
+    //salvar os novos dados do usuário
     saveUserData(existUsers);
     res.send({success: true, msg: 'User data added successfully'})
 })
-/* Read - GET method */
+
+/* Listar usuarios*/
 app.get('/user/list', (req, res) => {
     const users = getUserData()
     res.send(users)
 })
-/* Update - Patch method */
+
+/* Atualizar usuarios */
 app.patch('/user/update/:username', (req, res) => {
     //get the username from url
     const username = req.params.username
-    //get the update data
+    //obter os dados de atualização
     const userData = req.body
-    //get the existing user data
+    //obter os dados existentes do usuário
     const existUsers = getUserData()
-    //check if the username exist or not       
+
+    //verifique se o nome de usuário existe ou não      
     const findExist = existUsers.find( user => user.username === username )
     if (!findExist) {
         return res.status(409).send({error: true, msg: 'username not exist'})
     }
-    //filter the userdata
+    //filtrar os dados do usuário
     const updateUser = existUsers.filter( user => user.username !== username )
-    //push the updated data
+
+    //enviar os dados atualizados
     updateUser.push(userData)
-    //finally save it
+
+    //finalmente salvar o usuario atualizado
     saveUserData(updateUser)
     res.send({success: true, msg: 'User data updated successfully'})
 })
-/* Delete - Delete method */
+/* Deletar usuario */
 app.delete('/user/delete/:username', (req, res) => {
     const username = req.params.username
-    //get the existing userdata
+    //obter os dados de usuário existentes
     const existUsers = getUserData()
-    //filter the userdata to remove it
+    //filtrar os dados do usuário para removê-los
     const filterUser = existUsers.filter( user => user.username !== username )
     if ( existUsers.length === filterUser.length ) {
         return res.status(409).send({error: true, msg: 'username does not exist'})
     }
-    //save the filtered data
+    //salvar os dados filtrados
     saveUserData(filterUser)
     res.send({success: true, msg: 'User removed successfully'})
     
 })
+
+
 /* util functions */
-//read the user data from json file
+//ler os dados do usuário do arquivo json
 const saveUserData = (data) => {
     const stringifyData = JSON.stringify(data)
     fs.writeFileSync('users.json', stringifyData)
 }
-//get the user data from json file
+//obter os dados do usuário do arquivo json
 const getUserData = () => {
     const jsonData = fs.readFileSync('users.json')
     return JSON.parse(jsonData)    
 }
-/* util functions ends */
-//configure the server port
+/* Funções do servidor */
+//configurar a porta do servidor
 app.listen(3000, () => {
     console.log('Server runs on port 3000')
 })
